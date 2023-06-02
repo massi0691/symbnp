fdd4bd69cf96a9f525d1a64b3e41c3147e3ae327
massi9106 <massi9106.ait@gmail.com>
2023-06-02 10:44:57 +0200

    Mise en place de Encore pour nos CSS et JS

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const Encore = require('@symfony/webpack-encore');
-const {ProvidePlugin} = require("@symfony/webpack-encore/lib/plugins/plugin-priorities");
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
